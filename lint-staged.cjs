95a5b134011abeb651a2940c6427892c36f84a36
Nick Nyanjui <nicknyanjui@gmail.com>
2023-08-21 18:19:26 +0300

    feat(integrations): add cloudflare pages

--- lint-staged.cjs
+++ lint-staged.cjs
@@ -2,10 +2,9 @@ module.exports = {
     '*.{js,jsx,ts,tsx}': [
         'npm run fmt',
         'npm run lint',
-        'npm run test --bail --watchAll=false --findRelatedTests --passWithNoTests',
         () => 'tsc-files --noEmit',
     ],
     '*.{js,jsx,ts,tsx,json,css,js}': [
-        'prettier --write'
+        'npm run fmt'
     ],
 }
